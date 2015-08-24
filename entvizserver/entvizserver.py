e607e109387ffaafb61742ad5f6c8102c91160cd
Chawanat Nakasan <chawanat.n@gmail.com>
2015-08-24 18:13:52 +0900

    improved supporting functions & error handling

--- entvizserver/entvizserver.py
+++ entvizserver/entvizserver.py
@@ -3,6 +3,7 @@
 import web
 import ConfigParser
 import json
+import logging
 
 config_file = 'config.ini'
 
@@ -10,6 +11,13 @@ config_file_db_section = 'db'
 
 cp = ConfigParser.SafeConfigParser()
 cp.read(config_file)
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 #TODO: support postgres & sqlite
 db = web.database(dbn='mysql',
@@ -41,20 +49,49 @@ class switch_all:
 
 class domain:
     def GET(self):
-        allowed_columns = ['id', 'name', 'city', 'state', 'country']
-        query = dict(web.input())
-        results = db.select('domain', where=compose_where(allowed_columns, dict(web.input())))
+        allowed_columns = {'id', 'name', 'city', 'state', 'country'}
+        validate_query_fields(allowed_columns, dict(web.input()))
+        (q_vars, q_where) = vars_and_where(allowed_columns, dict(web.input()))
+        try:
+            results = db.select('domain', q_vars, where=q_where).list()
+        except Exception as e:
+            logger.exception(e)
+            return json.dumps({'errors': ['malformed query syntax']})
         web.header('Content-Type', 'application/json')
-        return json.dumps({'data': [x for x in results]})
+        if len(results) == 0:
+            return json.dumps({'data': [], 'warnings': ['query returned no results']})
+        else:
+            return json.dumps({'data': [x for x in results]})
+
+# UTILS: TODO: Move these into a separate file
+
+class ColumnNotAllowedError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def validate_query_fields(allowed_columns, query):
+    if not all([column in allowed_columns for column in query]):
+        raise ColumnNotAllowedError(set(query.keys()) - allowed_columns)
+    else:
+        return True
+
+def vars_and_where(columns, query):
+    return (filter_dict(columns, query), compose_where(columns, query))
+
+def filter_dict(columns, query):
+    return {column: query[column] for column in columns if column in query}
 
 def compose_where(columns, query):
     query_content = []
     for column in columns:
         if column in query:
-            query_content.append("%s='%s'" % (column, query[column]))
+            query_content.append("%s=$%s" % (column, column))
     return ' AND '.join(query_content)
 
 
 if __name__ == "__main__":
+    logger.info('Welcome to Summoner\'s Rift!')
     app = web.application(urls, globals())
     app.run()
